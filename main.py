ff39bf9cebfee7c1dd95c06abd41fcd60328f43c
Antonina Serdyukova <r66ff@hotmail.com>
2016-11-27 15:14:35 -0500

    not perfect but works, need some feedback:)

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys, random, string
 def random_gen():
     n = random.SystemRandom()
     n = n.getrandbits(1) # I thought getrandbits() would be a better solution than extracting the least significant bit
-    print('Extracting a random bit out of a random.SystemRandom object...\nExtracted random bit is: %s\n' % n)
+    print('Line # 6:\nExtracting a random bit out of a random.SystemRandom object...\nExtracted random bit is: %s\n' % n)
     return n
 
 def random_int():
@@ -33,11 +33,13 @@ def miller_rabin(n):
             for r in range(1, s):
                 x = pow(x, 2, n)
                 if x == 1:
+                    print('Line # 36:\nNot a prime: %s\n\n' % n)
                     return False  # this one is composite
                 elif x == n - 1:
-                    a = 0  # so we know loop didn't continue to end
+                    a = 0  # so we know loop didn't continue to end to not fall into 'if a:' statement that follows
                     break  # try another a
             if a:
+                print('Line # 42:\nNot a prime: %s\n\n' % n)
                 return False  # composite
     return True  # probably prime
 
@@ -48,7 +50,34 @@ def is_prob_prime():
     else:
         is_prob_prime()
 
-def main():
+def egcd(n, m):
+    pa, a = 1, 0
+    pb, b = 0, 1
+    while m:
+        k = n // m
+        a, pa = pa - k * a, a
+        b, pb = pb - k * b, b
+        n, m = m, n % m
+    return n, pa, pb
+
+def find_e(phi):
+    e = 3
+    g, x, y = egcd(phi, e)
+    print('\nLine # 66:\n g = %s, x = %s, y = %s\n\n' % (g, x, y))
+    while g != 1:
+        e = e + 1
+        g, x, y = egcd(phi, e)
+        print('\nLine # 70:\n g = %s, x = %s, y = %s\n\n' % (g, x, y))
+    return e
+
+def find_d(n, m):
+    g, x, y = egcd(n, m)
+    if g != 1:
+        raise Exception('Private key d can not be found')
+    else:
+        return x % m
+
+def generate_p_q():
     p = is_prob_prime()
     q = is_prob_prime()
     # check if overflow happened and check equality
@@ -56,8 +85,43 @@ def main():
         p = is_prob_prime()
     while (q == None or p == q):
         q = is_prob_prime()
+    return p, q
 
-    print(p)
-    print(q)
+def generate_p_q_n_phi_e():
+    p, q = generate_p_q()
+
+    print('Line # 93:\nPrime perhaps.. %s' % p)
+    print('Prime perhaps.. %s' % q)
+
+    n = p * q
+    phi = (p - 1) * (q - 1)
+    e = find_e(phi)
+
+    return p, q, n, phi, e
+
+def main():
+    p, q, n, phi, e = generate_p_q_n_phi_e()
+
+    # I don't think e would ever happen to be undefined in this implementation, but just per the instructions including this check
+    # Also I'm lame in unit testing and this code won't be sufficient if exception occures, but please give me feedback on how to this right:)
+    while (e == None):
+            p, q, n, phi, e = generate_p_q_n_phi_e()
+
+    print('Line # 110:\nP is: %s\nQ is: %s\nN is: %s\nE is: %s' % (p, q, n, e))
+
+    d = find_d(e, phi)
+
+    print('D is: %s' % d)
+    print("Alice's public key is (%s, %s)" % (n, e))
+    print("Alice's private key is (%s, %s)" % (n, d))
+
+    p = '{0:032b}'.format(p)
+    q = '{0:032b}'.format(q)
+    n = '{0:032b}'.format(n)
+    e = '{0:032b}'.format(e)
+    d = '{0:032b}'.format(d)
+    print('\nNow same in binary:\nP is: %s\nQ is: %s\nN is: %s\nE is: %s\nD is: %s' % (p, q, n, e, d))
+    print("Alice's public key is (%s, %s)" % (n, e))
+    print("Alice's private key is (%s, %s)" % (n, d))
 
 main()
